ec088797534ab3039cc748f6867ac909cfaddf3d
dige <apdyphitah1>
2026-02-04 21:43:41 +0300

    add all project files

---
+++ index.js
@@ -0,0 +1,6 @@
+const buttonsEl = document.querySelectorAll("button");
+for (let i = 0; i < buttonsEl.length; i++) {
+    buttonsEl[i].addEventListener("click", () =>{
+        console.log(buttonsEl[i].textContent);
+    })
+}
