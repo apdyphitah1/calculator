2622ae148f1a14eba851344c69d5f59cb826222d
dige <maqdashaawi@gmail.com>
2026-02-05 11:38:19 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 const buttonsEl = document.querySelectorAll("button");
+
+const inputfield = document.getElementById("result");
+
 for (let i = 0; i < buttonsEl.length; i++) {
-    buttonsEl[i].addEventListener("click", () =>{
-        console.log(buttonsEl[i].textContent);
-    })
+    buttonsEl[i].addEventListener("click",  () => { 
+        const buttonValue = buttonsEl[i].textContent;
+        console.log(buttonValue);
+        
+
+        if (buttonValue === "C") {
+            clearResult();
+        }
+        else if (buttonValue === "=") {
+            calculateResult();
+        }
+        else {
+            appendValue(buttonValue);
+            
+        }
+       
+       
+});
+ 
+}
+
+function clearResult() {
+    
+    inputfield.value = ""; 
+}
+
+
+function calculateResult() {
+    inputfield.value = eval(inputfield.value);
+     
+}
+
+
+
+function appendValue(buttonValue) {
+    inputfield.value += buttonValue;    
+
 }
